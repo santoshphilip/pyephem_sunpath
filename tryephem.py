73f7c7ae8b1a5b7f21b367d6bd2ec031ebbd3939
santoshphilip <santosh_philip@yahoo.com>
2018-05-05 14:36:50 -0700

    modified:   tryephem.py

--- tryephem.py
+++ tryephem.py
@@ -4,11 +4,21 @@ http://rhodesmill.org/pyephem/tutorial.html#computations-for-particular-observer
 
 
 import ephem
+import datetime
 
 gatech = ephem.Observer()
 gatech.lon, gatech.lat = '-84.39733', '33.775867'
 
-gatech.date = '1984/5/30 16:22:56'   # 12:22:56 EDT
+# gatech.date = '1984/5/30 16:22:56'   # 12:22:56 EDT
+# sun, moon = ephem.Sun(), ephem.Moon()
+# sun.compute(gatech)
+# moon.compute(gatech)
+# print("%s %s" % (sun.alt, sun.az))
+# # 70:08:39.2 122:11:26.4
+# print("%s %s" % (moon.alt, moon.az))
+# # 70:08:39.5 122:11:26.0
+
+gatech.date = datetime.datetime(1984, 5, 30, 16, 22, 56)
 sun, moon = ephem.Sun(), ephem.Moon()
 sun.compute(gatech)
 moon.compute(gatech)
